0a5e24e8e00a12ca0ac76c2620b30f6a5e516f12
AmitParihar17 <amitparihar1208@gmail.com>
2025-06-17 16:41:08 +0530

    updated

--- Chapter1/conditionalS.js
+++ Chapter1/conditionalS.js
@@ -6,7 +6,7 @@
 //     console.log("Please enter a valid input")
 // }
 // else if(age >= 18){
-//     console.log("Ypu are valid voter because your age is ->",age);
+//     console.log("You are valid voter because your age is ->",age);
 // }else{
 //     console.log("You can't vote because your age is less than 18  and your age is ->",age);
     
@@ -27,5 +27,72 @@
 //     // console.log(amount - Math.floor((20 * amount) / 100 ));
 // }  console.log(amount - Math.floor((disc * amount) / 100));
 
+
+// let unit = Number(prompt("Enter electercity unit")) ;//700
+// let amount = 0;
+
+// if (unit > 400) {
+//  amount += ( unit-400) * 13
+//  unit = 400
+    
+// } if(unit > 200 && unit <= 401){
+//     amount += (unit - 200) * 8
+//     unit = 200
+// } if(unit > 100 && unit < 201){
+//         amount += (unit-100) * 6
+//         unit = 100
+// }
+// amount += unit  * 4
+// console.log(amount);
+
+// let amount = 5001;
+// if(amount >= 500){
+//     console.log("500 notes -",Math.floor(amount / 500));
+//    amount = amount % 500
+// } if(amount >= 200){
+//     console.log("200 otes -",Math.floor(amount / 200));
+//     amount = amount  % 200
+// } if (amount >= 100) {
+//     console.log("100 notes -",Math.floor(amount / 100));
+//         amount = amount % 100
+// }  if (amount >= 50) {
+//   console.log("50 notes -", Math.floor(amount / 50));
+//   amount = amount % 50;
+// } if (amount >= 20) {
+//   console.log("20 notes -", Math.floor(amount / 20));
+//   amount = amount % 20;
+// } if (amount >= 10) {
+//   console.log("10 notes -", Math.floor(amount / 10));
+//   amount = amount % 10;
+// } if (amount >=5) {
+//   console.log("5 notes -", Math.floor(amount / 5));
+//   amount = amount % 5;
+// } if (amount >= 2) {
+//   console.log("2 notes -", Math.floor(amount / 2));
+//   amount = amount % 2;
+// } if(amount === 1){
+//     console.log("1 notes" , amount);
+    
+// }
+
+
+// console.log(21 > 3 ? "yes" : "no"); ternary
+
+// let num = 0;
+// console.log(num > 0 ? "positive" : num < 0 ? "negative" : "zero" ); nested trenary
+
+//switch
+
+let day = 7;
+switch(day){
+    case 1 : console.log("Monday");
+    break;
+    case 2 : console.log("Tuesday");
+    break;
+    default : console.log("Invalid");
+    
+}
+
+ 
 
 
