351232b2a8d45721dc43f25de34da4b6770c624f
AmitParihar17 <amitparihar1208@gmail.com>
2025-06-17 14:13:32 +0530

    chapter2

---
+++ Chapter1/conditionalS.js
@@ -0,0 +1,31 @@
+// let age = prompt("Enter your age")
+// console.log(typeof age);
+// age = Number(age)
+// console.log(typeof age);
+// if(isNaN(age)){
+//     console.log("Please enter a valid input")
+// }
+// else if(age >= 18){
+//     console.log("Ypu are valid voter because your age is ->",age);
+// }else{
+//     console.log("You can't vote because your age is less than 18  and your age is ->",age);
+    
+// }
+
+// let amount =Number(prompt("What is the final amount ?"));
+// let disc = 0;
+// if(amount > 0 && amount <= 5000){
+//     disc = 0
+// } else if(amount > 5000 && amount <= 7000){
+//     // console.log(amount - Math.floor((5 * amount) / 100 ));/
+//     disc = 5
+// } else if(amount > 7000 && amount <= 9000){
+//     disc = 10
+//     // console.log(amount - Math.floor((10 * amount) / 100 ));
+// }else if(amount > 9000){
+//     disc = 20
+//     // console.log(amount - Math.floor((20 * amount) / 100 ));
+// }  console.log(amount - Math.floor((disc * amount) / 100));
+
+
+
