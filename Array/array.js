c3e244ca8323249d22d35f2275c9b6e4194e23ef
AmitParihar17 <amitparihar1208@gmail.com>
2025-06-26 15:58:40 +0530

    leraning array

---
+++ Array/array.js
@@ -0,0 +1,35 @@
+// let arr = []
+// arr[0]=5
+// arr[1]=4
+// arr[2]= 3
+// arr[4]=2
+// arr[1]=0
+// arr[5]=7
+
+// arr[10]=10
+// console.log(arr);
+
+// let arr = new Array(3)
+// arr[0]=7
+// arr[1]=9
+// arr[2]=5
+// arr[3]=undefined
+// console.log(arr);
+
+// let arr = new Array(5)
+// for(let i=0 ; i<arr.length;i++){
+//     arr[i] = Number(prompt("Enter a number"))
+// }
+// console.log(arr);
+
+
+let arr =[10,20,30,40,50]
+let sum = 0;
+for(let i=0;i<arr.length;i++){
+    sum += arr[i] 
+}
+console.log(arr);
+console.log(sum);
+
+
+
--- Chapter1/conditionalS.js
+++ Chapter1/conditionalS.js
@@ -83,15 +83,15 @@
 
 //switch
 
-let day = 7;
-switch(day){
-    case 1 : console.log("Monday");
-    break;
-    case 2 : console.log("Tuesday");
-    break;
-    default : console.log("Invalid");
+// let day = 7;
+// switch(day){
+//     case 1 : console.log("Monday");
+//     break;
+//     case 2 : console.log("Tuesday");
+//     break;
+//     default : console.log("Invalid");
     
-}
+// }
 
  
 
