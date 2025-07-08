b72d0b762c997d925863f771cc5249bd69069d31
AmitParihar17 <amitparihar1208@gmail.com>
2025-07-08 22:51:11 +0530

    hoisting & clousers

---
+++ Chapter1/hoisting.js
@@ -0,0 +1,60 @@
+// console.log(a);
+// var a = 10;
+
+// hello()
+
+// function hello(){
+//     console.log("hello bahi g gg");
+
+// }\
+
+// sayBhai()
+
+// var sayBhai = function(){
+//     console.log("bhai");
+
+// }
+
+// sayBhai()
+
+// console.log(a);
+// let a= 10
+
+// const a = 0;
+
+// console.log(a);
+
+// CLOUSRES
+
+// function outer(){
+//     let count = 2;
+
+//     return function inner(){
+//         count++
+//         console.log(count);
+//     }
+// }
+// const clousre = outer()
+// clousre()
+// clousre()
+// clousre()
+
+// function createCounter() {
+//     let count = 0;
+
+//     return function () {
+//       count++;
+//       console.log(count);
+//     };
+//   }
+
+//   const counter1 = createCounter(); // First counter
+//   const counter2 = createCounter(); // Second counter
+
+//   counter1(); // 1
+//   counter1(); // 2
+
+//   counter2(); // 1
+//   counter2(); // 2
+//   counter1(); // 3
+
